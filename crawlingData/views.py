75090caf369ac2f7033eecdd70fd1a4a165d72b1
System Administrator <root@Lee-ui-MacBook-Air.local>
2014-01-24 00:23:16 +0900

    add app

--- crawlingData/views.py
+++ crawlingData/views.py
@@ -2,4 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse("첫번째 View입니다.")
+    output = '''
+    <html>
+        <head>
+            first view
+        </head>
+        <body>
+            first view
+        </body>
+    </html>
+    '''
+    
+    return HttpResponse(output)
