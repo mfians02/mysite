ce7f04b1167c20684f7ce938ae61666fe9607aac
System Administrator <root@Lee-ui-MacBook-Air.local>
2014-02-05 23:47:15 +0900

    db

--- crawlingData/models.py
+++ crawlingData/models.py
@@ -4,27 +4,25 @@ from django.db import models
 
 # Create your models here.
 
-class Xth(models.Model):
+class Odd(models.Model):
     def __unicode__(self):
-        return self.number
-    number = models.IntegerField()
-    game = models.ForeignKey(Game)
+        return self.serviceProvider
+    serviceProvider = models.TextField(max_length=50)
+    winP = models.DecimalField()
+    drawP = models.DecimalField()
+    loseP = models.DecimalField()
 
 class Game(models.Model):
     def __unicode__(self):
         return self.gameNumber
-    number = models.ForeignKey(Xth)
     gameNumber = models.IntegerField()
     category = models.TextField(max_length=50)
     league = models.TextField(max_length=50)
     date = models.DateTimeField()
     odd = models.ForeignKey(Odd)
-    
-class Odd(models.Model):
+
+class Xth(models.Model):
     def __unicode__(self):
-        return self.serviceProvider
+        return self.number
+    number = models.IntegerField()
     game = models.ForeignKey(Game)
-    serviceProvider = models.TextField(max_length=50)
-    winP = models.DecimalField()
-    drawP = models.DecimalField()
-    loseP = models.DecimalField()
